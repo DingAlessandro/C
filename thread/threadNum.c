2aaf35e8e205bfea45c8f20bf8272e3b48ac14ae
DingAlessandro <121311960+DingAlessandro@users.noreply.github.com>
2024-04-18 08:37:10 +0200

    Update threadNum.c

--- thread/threadNum.c
+++ thread/threadNum.c
@@ -19,8 +19,8 @@ int main() {
     pthread_t thread1, thread2;
     int range1[2] = {0, 4};
     int range2[2] = {5, 9};
-    pthread_create(&thread1, NULL, search, (void*)&range1);
-    pthread_create(&thread2, NULL, search, (void*)&range2);
+    pthread_create(&thread1, NULL, &search, (void*)&range1);
+    pthread_create(&thread2, NULL, &search, (void*)&range2);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     printf("Il numero otto si trova alla posizione: %d\n", position);
